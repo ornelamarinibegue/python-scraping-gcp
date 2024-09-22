e5e4399716f27a80d9aa7a38af19dc56864382f5
ornelamarinibegue <ornelamarinibegue1983@gmail.com>
2024-09-22 19:17:30 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,14 @@
 # Construir la imagen
 docker build -t python_scraping_project .
 
-# Aquí puedes añadir el comando para subir a un repositorio de artefactos
+# Autenticar con GCR
+gcloud auth configure-docker
+
+# Etiquetar la imagen
+docker tag python_scraping_project gcr.io/your-project-id/python_scraping_project
+
+# Subir la imagen a GCR
+docker push gcr.io/your-project-id/python_scraping_project
 
 # Desplegar en Cloud Run
 gcloud run deploy python_scraping_project --image gcr.io/your-project-id/python_scraping_project --platform managed
