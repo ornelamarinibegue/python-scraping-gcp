327d5766da9050320b30fbebb6cdeb97bead212a
Ornela Marini <omarini@teco.com.ar>
2024-09-30 17:58:14 -0300

    agregue la libreria EC

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from google.cloud import bigquery
 
